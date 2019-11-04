742c3d438f3156c64904afc1e7b268371aedb7ee
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-04 15:55:17 +0000

    created a get endpoint to fetch accounts

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
-const db = require('../data/dbConfig');
+const db = require("../data/dbConfig");
 
 const accountRouter = express.Router();
 
-accountRouter.get("/", (req, res) => {});
+accountRouter.get("/", (req, res) => {
+  db.select('*')
+    .from('accounts')
+    .then(accounts => {
+      res.status(200).json(accounts);
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message
+      });
+    });
+});
 
 module.exports = accountRouter;
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const accountRouter = require('./accounts/account-router')
 
-// const db = require('./data/dbConfig.js');
+const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
 
-server.use('api/accounts', accountRouter);
+server.use('/api/accounts', accountRouter);
 
 server.get('/', (req, res) => {
     res.send('<h3> Accounts </h3>');
