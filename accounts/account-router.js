b2d89c0e2c03f3dd8912dcc4a0b5c756c39a381e
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-04 16:10:41 +0000

    created a get endpoint to fetch account with specific id

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -5,8 +5,8 @@ const db = require("../data/dbConfig");
 const accountRouter = express.Router();
 
 accountRouter.get("/", (req, res) => {
-  db.select('*')
-    .from('accounts')
+  db.select("*")
+    .from("accounts")
     .then(accounts => {
       res.status(200).json(accounts);
     })
@@ -15,6 +15,22 @@ accountRouter.get("/", (req, res) => {
         message: error.message
       });
     });
+});
+
+accountRouter.get("/:id", async (req, res) => {
+  try {
+    const account = await db
+      .select("*")
+      .where({ id: req.params.id })
+      .from("accounts");
+    res.status(200).json({
+      account
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
 });
 
 module.exports = accountRouter;
